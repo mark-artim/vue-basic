d69354fd9a996de7156a1ffbd731eb6a90d70781
mark-artim <mark.artim@gmail.com>
2025-06-20 09:30:04 -0500

    revised cors logic

--- auth-backend/app.js
+++ auth-backend/app.js
@@ -16,8 +16,8 @@ const app = express()
 // CORS configuration
 const allowedOrigins = [
   'http://localhost:3000', // for local dev
-  'https://vue-basic-flame.vercel.app/',  // ✅ for production
-  'https://vue-basic-mark-artims-projects.vercel.app/',
+  'https://vue-basic-flame.vercel.app',  // ✅ for production
+  'https://vue-basic-mark-artims-projects.vercel.app',
 ];
 
 app.use(cors({
